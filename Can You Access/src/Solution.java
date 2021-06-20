993f6c7d389159021d8419496b898b34da50a9cb
Nafis Fuad <nafisfuad1158015@gmail.com>
2021-06-20 12:42:35 +0600

    Lamda Expression

--- Can You Access/src/Solution.java
+++ Can You Access/src/Solution.java
@@ -1,6 +1,26 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/*
+ * problem: https://www.hackerrank.com/challenges/can-you-access/problem
+ * 
+ * You are given a class Solution and an inner class Inner.Private. The main method of class Solution takes an integer as input. The powerof2 in class Inner.Private checks whether a number is a power of
+
+. You have to call the method powerof2 of the class Inner.Private from the main method of the class Solution.
+
+Constraints
+
+Sample Input
+
+8
+
+Sample Output
+
+8 is power of 2
+An instance of class: Solution.Inner.Private has been created
+
+ * */
+
 public class Solution {
 
 	public static void main(String[] args) throws Exception {
