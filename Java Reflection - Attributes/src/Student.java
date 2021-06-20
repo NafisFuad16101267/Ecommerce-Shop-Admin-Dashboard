ba0b0145f50d25779ab68d067cf3da3bdee8eafa
Nafis Fuad <nafisfuad1158015@gmail.com>
2021-06-20 16:04:55 +0600

    Java Reflection-Attribute

---
+++ Java Reflection - Attributes/src/Student.java
@@ -0,0 +1,33 @@
+
+public class Student {
+	public void ahqym(){}
+	public void amftc(){}
+	public void anotherfunction(){}
+	public void atcks(){}
+	public void bwkbd(){}
+	public void cfwyc(){}
+	public void cmkxa(){}
+	public void dnpym(){}
+	public void dnqvo(){}
+	public void dvvwq(){}
+	public void ehjdm(){}
+	public void elyed(){}
+	public void fmyce(){}
+	public void getEmail(){}
+	public void getId(){}
+	public void getName(){}
+	public void ghtlj(){}
+	public void hluvb(){}
+	public void isqdf(){}
+	public void iwhtf(){}
+	public void jmopy(){}
+	public void jnskt(){}
+	public void kbjlt(){}
+	public void kgwku(){}
+	public void khuag(){}
+	public void levtp(){}
+	public void mcgme(){}
+	public void migyc(){}
+	public void moebl(){}
+	
+}
